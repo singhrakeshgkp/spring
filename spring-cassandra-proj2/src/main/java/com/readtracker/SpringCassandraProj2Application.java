ae68482ad1c5e3ad36bf31362e476e41da6c9ff1
Rakesh Singh <rakesh-sin@HCL.COM>
2023-03-16 16:31:49 -0700

    added cassandra proj2

---
+++ spring-cassandra-proj2/src/main/java/com/readtracker/SpringCassandraProj2Application.java
@@ -0,0 +1,13 @@
+package com.readtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraProj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraProj2Application.class, args);
+	}
+
+}
---
+++ spring-cassandra-proj2/src/test/java/com/readtracker/SpringCassandraProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCassandraProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
